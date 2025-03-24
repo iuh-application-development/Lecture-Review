394a8d7aef6678dcedaf303d6fc4f8a60445abf0
thanhthao08 <22695701.thao@student.iuh.edu.vn>
2025-03-24 13:49:29 +0700

    Commit

--- website/views.py
+++ website/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template
-from flask_login import current_user, login_required
+from flask_login import current_user
 
 views = Blueprint('views', __name__)
 
@@ -8,11 +8,13 @@ def home():
     return render_template('home.html', user=current_user)
 
 @views.route('/dashboard')
-@login_required
 def dashboard():
     return render_template('dashboard.html', user=current_user)
 
-@views.route('/all-my-notes')
-@login_required
-def all_my_notes():
-    return render_template('all_my_notes.html', user=current_user)
+@views.route('/share-with-me')
+def share_with_me():
+    return render_template('share_with_me.html', user=current_user)
+
+@views.route('/trash')
+def trash():
+    return render_template('trash.html', user=current_user)
